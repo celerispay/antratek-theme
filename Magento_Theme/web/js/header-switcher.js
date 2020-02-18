cc388137962c4a300c4bafb429306178c63cba62
Charchit Khandelwal <charchit@newgenpayments.com>
2020-02-18 18:43:07 +0530

    [Feature][Update] tax vat switcher
    Signed-off-by: Charchit Khandelwal <charchit@newgenpayments.com>

--- Magento_Theme/web/js/header-switcher.js
+++ Magento_Theme/web/js/header-switcher.js
@@ -2,15 +2,30 @@ require(['jquery'], function ($) {
     'use strict';
     $("document").ready(function () {
         var list_childs = $('.nav-item.level0.level-top.right.b2c-checkbox').children();
-        $("input#check").click(function () {
-            if ($('input#check').is(":checked")) {
+        var inp = $("#check");
+        var sel = $(".tax_display_select");
+        var switcherVal = sel.val();
+        if (switcherVal == 1) {
+            inp.prop("checked", true);
+            $(list_childs[2]).removeClass('onload-color');
+            $(list_childs[0]).addClass('onload-color');
+        } else {
+            inp.prop("checked", false);
+            $(list_childs[2]).addClass('onload-color');
+            $(list_childs[0]).removeClass('onload-color');
+        }
+        $(inp).change(function () {
+            var checked = inp.prop("checked");
+            sel.val(checked ? 1 : 2);
+            if (checked) {
                 $(list_childs[2]).removeClass('onload-color');
                 $(list_childs[0]).addClass('onload-color');
             } else {
                 $(list_childs[2]).addClass('onload-color');
                 $(list_childs[0]).removeClass('onload-color');
             }
-        })
+            sel.trigger("change");
+        });
     });
 });
 
