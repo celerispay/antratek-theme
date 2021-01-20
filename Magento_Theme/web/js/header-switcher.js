5f4a60e1a4d9b609458c0920ca6b57a0e45bdaab
charchit khandelwal <charchit@newgenpayments.com>
2021-01-20 18:22:51 +0530

    [Update][Bugfix] price tax switcher session store and js implementations
    Signed-off-by: charchit khandelwal <charchit@newgenpayments.com>

--- Magento_Theme/web/js/header-switcher.js
+++ Magento_Theme/web/js/header-switcher.js
@@ -2,20 +2,38 @@ require(['jquery'], function($) {
     'use strict';
     $("document").ready(function() {
         var list_childs = $('.nav-item.level0.level-top.right.b2c-checkbox').children();
-        $("#check").click(function() {
-            if ($('#check').is(":checked")) {
-                $(list_childs[2]).removeClass('onload-color');
-                $(list_childs[0]).addClass('onload-color');
-                $('.price-including-tax').css("display", "table-footer-group");
-                $('.price-including-tax .price').css("font-size", "1.6rem");
-                $('.price-excluding-tax .price').css("font-size", "2.2rem");
+        var switchState = localStorage.getItem('tax-switch');
+        if (switchState == "true") {
+            priceSwitchActive();
+        } else {
+            priceSwitchDeactive();
+        }
+        $('#check').click(function() {
+            if ($('#check').is(":checked") == true) {
+                priceSwitchActive();
             } else {
-                $(list_childs[2]).addClass('onload-color');
-                $(list_childs[0]).removeClass('onload-color');
-                $('.price-including-tax').css("display", "table-header-group");
-                $('.price-including-tax .price').css("font-size", "2.2rem");
-                $('.price-excluding-tax .price').css("font-size", "1.6rem");
+                priceSwitchDeactive();
             }
         });
+
+        function priceSwitchActive() {
+            localStorage.setItem('tax-switch', true);
+            $('#check').prop('checked', true);
+            $(list_childs[2]).removeClass('onload-color');
+            $(list_childs[0]).addClass('onload-color');
+            $('.price-including-tax').css("display", "table-footer-group");
+            $('.price-including-tax .price').css("font-size", "1.6rem");
+            $('.price-excluding-tax .price').css("font-size", "2.2rem");
+        }
+
+        function priceSwitchDeactive() {
+            localStorage.setItem('tax-switch', false);
+            $(list_childs[2]).addClass('onload-color');
+            $(list_childs[0]).removeClass('onload-color');
+            $('.price-including-tax').css("display", "table-header-group");
+            $('.price-including-tax .price').css("font-size", "2.2rem");
+            $('.price-excluding-tax .price').css("font-size", "1.6rem");
+        }
+
     });
 });
