e5401783a8b4733199ac380228dae2c0e569b63a
charchit khandelwal <charchit@newgenpayments.com>
2021-03-18 13:26:19 +0530

    [Bugfix] tax vat switcher jquery functions
    Signed-off-by: charchit khandelwal <charchit@newgenpayments.com>

--- Magento_Theme/web/js/header-switcher.js
+++ Magento_Theme/web/js/header-switcher.js
@@ -2,30 +2,32 @@ require(['jquery'], function($) {
     'use strict';
     $("document").ready(function() {
         var list_childs = $('.nav-item.level0.level-top.right.b2c-checkbox').children();
-        var switchState = localStorage.getItem('tax-switch');
+        var switchState = JSON.parse(localStorage.getItem('tax-switch'));
+        priceSwitch(switchState);
         var interval = undefined;
         var counter = 0;
         interval = setInterval(function() {
             counter += 1;
-            var check = $('.block.aw_wbtab .slick-list').length > 0;
+            var check = $('.block.aw_wbtab .slick-list').length > 0 || $('#checkout-shipping-method-load').length > 0;
             if (check) {
-                if (switchState == "true") {
-                    priceSwitchActive();
-                } else {
-                    priceSwitchDeactive();
-                }
+                priceSwitch(switchState);
             }
             if (check || counter >= 250) {
                 clearInterval(interval);
             }
         }, 200)
+
         $('#check').click(function() {
-            if ($('#check').is(":checked") == true) {
+            $('#check').is(":checked") ? priceSwitch(true) : priceSwitch(false);
+        });
+
+        function priceSwitch(currState) {
+            if (currState == true) {
                 priceSwitchActive();
             } else {
                 priceSwitchDeactive();
             }
-        });
+        }
 
         function priceSwitchActive() {
             localStorage.setItem('tax-switch', true);
