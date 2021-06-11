7237e47abd68f9b82f9171bf749d71a005ea3f60
charchit khandelwal <charchit@newgenpayments.com>
2021-06-11 19:06:25 +0530

    [Update] logxstar shipping js hideshipping method
    Signed-off-by: charchit khandelwal <charchit@newgenpayments.com>

--- Logxstar_Integration/web/js/view/cart/logxstarpickup.js
+++ Logxstar_Integration/web/js/view/cart/logxstarpickup.js
@@ -619,20 +619,18 @@ define(
                 var hideTimer = setTimeout(function() {
                     component.getServicePoints();
 
-                    if ($('#co-shipping-method-form input[value$="__lgxspickup"]').length > 1) {
-                        $('#co-shipping-method-form input[value$="__lgxspickup"]').closest('.field.choice.item').hide();
+                    if ($('input[value$="__lgxspickup"]').length > 1) {
+                        $('input[value$="__lgxspickup"]').closest('.field.choice.item').hide();
                         var selected_el = jQuery('#co-shipping-method-form')
                             .find('input[type="radio"][value^="logxstar"]:checked:not([value$="__lgxspickup"])');
                         jQuery('#co-shipping-method-form')
-                            .find('input[type="radio"]:not([value$="__lgxspickup"])')
-                            .parent()
-                            .parent()
+                            .find('#pickup_mock_up')
                             .on('click', function() {
                                 $('#pickup_mock_up').find('input').prop('checked', false);
                             });
                         if (component.mocked == false || selected_el.length > 0) {
                             jQuery('#pickup_mock_up').remove();
-                            $('#co-shipping-method-form dl.items.methods dd.item-options:nth-of-type(2)').append(
+                            $('#co-shipping-method-form').append(
                                 '<div id="pickup_mock_up">' +
                                 '<div id="deliver_pickup" class="selectPickuppoint">' +
                                 '<table class="table-checkout-shipping-method">' +
